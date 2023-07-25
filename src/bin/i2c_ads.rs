bc4befd5f91718c2733d92ef70fe5047efa2a63b
schmettow <schmettow@web.de>
2023-07-25 15:48:21 +0200

    + async reading + reading all four pins

--- src/bin/i2c_ads.rs
+++ src/bin/i2c_ads.rs
@@ -9,8 +9,7 @@
 
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
-use embassy_rp::i2c::{self, Config, InterruptHandler};
-use embassy_rp::peripherals::I2C1;
+
 use embassy_time::{Duration, Ticker};
 //use embedded_hal_async::i2c::I2c;
 use {defmt_rtt as _, panic_probe as _};
@@ -19,49 +18,56 @@ bind_interrupts!(struct Irqs {
     I2C1_IRQ => InterruptHandler<I2C1>;
 });
 
-use embedded_ads111x as ads111x;
 
+
+use embassy_rp::i2c::{self, Config, InterruptHandler};
+use embassy_rp::peripherals::{PIN_14, PIN_15, I2C1};
+use embedded_ads111x as ads111x;
+use embedded_ads111x::InputMultiplexer::{AIN0GND, AIN1GND, AIN2GND, AIN3GND};
 
 #[embassy_executor::task]
-async fn adc_task(mut i2c: i2c::I2c<'_, I2C1, i2c::Async>, 
+async fn ads_task(i2c: i2c::I2c<'static, I2C1, i2c::Async>,
                   hz: u64) {
+    let config = 
+        ads111x::ADS111xConfig::default()
+        .dr(ads111x::DataRate::SPS8)
+        .pga(ads111x::ProgramableGainAmplifier::V4_096);
+    
+    let mut ads: ads111x::ADS111x<i2c::I2c<'_, I2C1, i2c::Async>> = 
+        ads111x::ADS111x::new(i2c,
+                              0x48u8, config).unwrap();
+    
     let mut ticker = Ticker::every(Duration::from_hz(hz));
     loop {
-        let res = adc.read(&mut adc_0).await;
-        RESULT.signal(res);
-        let res = adc.read(&mut adc_1).await;
-        RESULT.signal(res);
-        let res = adc.read(&mut adc_2).await;
-        RESULT.signal(res);
-        let res = adc.read(&mut adc_3).await;
-        RESULT.signal(res);
+        //config
+        // .mux(AIN0GND);
+        //ads.write_config(config);
+        // let _reading = ads.read_voltage().unwrap();
+        let _reading = ads.read_single_voltage(Some(AIN0GND)).unwrap();
+        let _reading = ads.read_single_voltage(Some(AIN1GND)).unwrap();
+        let _reading = ads.read_single_voltage(Some(AIN2GND)).unwrap();
+        let _reading = ads.read_single_voltage(Some(AIN3GND)).unwrap();
         ticker.next().await;
-         }
+    }
                            }
 
 
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let sda = p.PIN_14;
-    let scl = p.PIN_15;
+    let sda: PIN_14 = p.PIN_14;
+    let scl: PIN_15 = p.PIN_15;
+    let i2c: i2c::I2c<'_, I2C1, i2c::Async> = 
+        i2c::I2c::new_async(p.I2C1, 
+                            scl, sda, 
+                            Irqs, 
+                            Config::default());
+    let mut ticker = Ticker::every(Duration::from_hz(10));
 
-    let i2c: i2c::I2c<'_, I2C1, i2c::Async> = i2c::I2c::new_async(p.I2C1, scl, sda, Irqs, Config::default());
-    
-    let config = ads111x::ADS111xConfig::default()
-        .mux(ads111x::InputMultiplexer::AIN0GND)
-        .dr(ads111x::DataRate::SPS8)
-        .pga(ads111x::ProgramableGainAmplifier::V4_096);
-
-
-    let mut adc = 
-        ads111x::ADS111x::new(i2c, 0x48u8, config).unwrap();
-    
-    let mut ticker = Ticker::every(Duration::from_hz(500));
+    spawner.spawn(ads_task(i2c, 5000)).unwrap();
 
     loop {
-        let _reading = adc.read_single_voltage(None).unwrap();
         ticker.next().await;
     }
 
