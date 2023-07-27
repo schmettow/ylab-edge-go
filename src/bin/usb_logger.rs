c39dc8026b784ba33678818776805bd616d63c4a
schmettow <schmettow@web.de>
2023-07-27 15:44:26 +0200

    + moved USB device setup into the logger_task + moved the logger task into a module

--- src/bin/usb_logger.rs
+++ src/bin/usb_logger.rs
@@ -11,33 +11,42 @@ use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
-/* USB Logging */
-use embassy_rp::bind_interrupts;
-use embassy_rp::peripherals::USB;
-use embassy_rp::usb::{Driver, InterruptHandler};
-use log::{LevelFilter};
-use embassy_usb_logger;
-bind_interrupts!(struct Irqs {
-    USBCTRL_IRQ => InterruptHandler<USB>;
-});
 
-#[embassy_executor::task]
-async fn logger_task(driver: Driver<'static, USB>) {
-    // This is copied from the crate. The run! macro is using set_mac_level, where the racy version is needed.
-        static LOGGER: embassy_usb_logger::UsbLogger<1024> = embassy_usb_logger::UsbLogger::new();
+mod usb_logger {
+    /* USB Logging */
+    use embassy_rp::bind_interrupts;
+    use embassy_rp::peripherals::USB;
+    use embassy_rp::usb::{Driver, InterruptHandler};
+    use log::{LevelFilter};
+    use embassy_usb_logger;
+
+    #[embassy_executor::task]
+    pub async fn logger_task(usb: USB) {
+        bind_interrupts!(struct Irqs {
+            USBCTRL_IRQ => InterruptHandler<USB>;
+        });
+
+        let driver = 
+            Driver::new(usb, Irqs);
+        // This is copied from the crate. The run! macro is using set_max_level, where the racy version is needed.
+        static LOGGER: embassy_usb_logger::UsbLogger<1024> = 
+            embassy_usb_logger::UsbLogger::new();
         unsafe {
             let _ = log::set_logger_racy(&LOGGER)
                     .map(|()| log::set_max_level_racy(LevelFilter::Info));
         };
-        let _ = LOGGER.run(&mut embassy_usb_logger::LoggerState::new(), driver).await;
-    }
+        let _ = LOGGER.run(&mut embassy_usb_logger::LoggerState::new(), driver)
+                .await;
+        }
+}
+
 
 /* MAIN */
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let driver = Driver::new(p.USB, Irqs);
-    spawner.spawn(logger_task(driver)).unwrap();
+    spawner.spawn(usb_logger::logger_task(p.USB)).unwrap();
+    
     let mut counter = 0;
     loop {
         counter += 1;
