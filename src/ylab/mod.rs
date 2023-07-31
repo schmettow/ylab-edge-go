ee9c912b273b7bcec05d999aaff2a624ca7396cf
schmettow <schmettow@web.de>
2023-08-01 00:14:15 +0200

    + new module ytfc + added Reading type + started implementing Display

--- src/ylab/mod.rs
+++ src/ylab/mod.rs
@@ -1,7 +1,25 @@
 #![no_std]
 #![feature(type_alias_impl_trait)]
 
-pub mod ysns;
-pub mod yuio;
-pub mod yuii;
-pub mod ytf;
+pub mod ysns; // Ylab sensors
+pub mod yuio; // YLab UI Output
+pub mod yuii; // YLab UI Input
+pub mod ytfk; // YLab transfer formats & kodices
+
+use embassy_time::Instant;
+
+struct Reading {
+    time: Instant,
+    reading: [f32]
+}
+
+impl fmt::Display for Reading {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(f, "{}", self.0)
+    }
+}
