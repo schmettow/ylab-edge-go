27bbff1254fc9490dd84d609d5a178c2b4ee2787
schmettow <schmettow@web.de>
2023-08-01 00:13:02 +0200

    + added ytfk module with BSU submodule

---
+++ src/ylab/ytfk.rs
@@ -0,0 +1,49 @@
+/* YLab transport formats  */
+pub mod bsu {
+    /* USB Logging */
+    use embassy_rp::bind_interrupts;
+    use embassy_rp::peripherals::USB;
+    use embassy_rp::usb::{Driver, InterruptHandler};
+    use log::LevelFilter;
+    use embassy_usb_logger;
+
+    /* The task of saving data from multiple sources has the exact same straucture
+    as the logging task.*/
+    #[embassy_executor::task]
+    pub async fn task(usb: USB) {
+        bind_interrupts!(struct Irqs {
+            USBCTRL_IRQ => InterruptHandler<USB>;
+        });
+
+        let driver = 
+            Driver::new(usb, Irqs);
+        // This is copied from the crate. The run! macro is using set_max_level, where the racy version is needed.
+        static LOGGER: embassy_usb_logger::UsbLogger<1024> = 
+            embassy_usb_logger::UsbLogger::new();
+        unsafe {
+            let _ = log::set_logger_racy(&LOGGER)
+                    .map(|()| log::set_max_level_racy(LevelFilter::Info));
+        };
+        let _ = LOGGER.run(&mut embassy_usb_logger::LoggerState::new(), driver)
+                .await;
+        }
+
+        
+        /* Main
+        use embassy_executor::Spawner;
+        use embassy_time::{Duration, Timer};
+        use {defmt_rtt as _, panic_probe as _};
+        #[embassy_executor::main]
+        async fn main(spawner: Spawner) {
+        let p = embassy_rp::init(Default::default());
+        spawner.spawn(task(p.USB)).unwrap();
+    
+        let mut counter = 0;
+        loop {
+            counter += 1;
+            log::info!("Tick {}", counter);
+            Timer::after(Duration::from_secs(1)).await;
+        }
+    } */
+}
+
