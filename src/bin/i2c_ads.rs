55464553795d9fdaba83f42b5f958c101d58a0f4
schmettow <schmettow@web.de>
2023-08-07 13:05:24 +0200

    + changed Pins to 3 and 2 + added BSU

--- src/bin/i2c_ads.rs
+++ src/bin/i2c_ads.rs
@@ -25,21 +25,21 @@ mod ads1115 {
     
     // I2C    
     use embassy_rp::i2c::{self, Config, InterruptHandler};
-    use embassy_rp::peripherals::{PIN_14, PIN_15, I2C1};
+    use embassy_rp::peripherals::{PIN_2, PIN_3, I2C1};
     use embassy_rp::bind_interrupts;
     use embedded_ads111x as ads111x;
     use embedded_ads111x::InputMultiplexer::{AIN0GND, AIN1GND, AIN2GND, AIN3GND};
     // ITC
     use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
     use embassy_sync::signal::Signal;
-    type Reading = (f32, f32, f32, f32);
+    type Reading = [f32;4];
     type Measure = SensorResult<Reading>;
     pub static RESULT:Signal<CriticalSectionRawMutex, Measure> = Signal::new();
 
     #[embassy_executor::task]
     pub async fn task(contr: I2C1, 
-                      sda: PIN_14, 
-                      scl: PIN_15,
+                      scl: PIN_3, 
+                      sda: PIN_2,
                       hz: u64) {
         // ads1115
         // Init I2C
@@ -63,10 +63,10 @@ mod ads1115 {
         let mut ticker = Ticker::every(Duration::from_hz(hz));
         loop {
             let reading: Reading =
-               (ads.read_single_voltage(Some(AIN0GND)).unwrap(),
+               [ads.read_single_voltage(Some(AIN0GND)).unwrap(),
                 ads.read_single_voltage(Some(AIN1GND)).unwrap(),
                 ads.read_single_voltage(Some(AIN2GND)).unwrap(),
-                ads.read_single_voltage(Some(AIN3GND)).unwrap());
+                ads.read_single_voltage(Some(AIN3GND)).unwrap()];
             let now = Instant::now();
             let result = 
                 Measure{time: now, reading: reading};       
@@ -80,9 +80,11 @@ mod ads1115 {
 /* MAIN */
 
 // I2C
-use embassy_rp::peripherals::{PIN_14, PIN_15};
+use embassy_rp::peripherals::{PIN_3, PIN_2};
 // LED
 use embassy_rp::gpio::{Output, Level};
+// BSU
+use ylab::ytfk::bsu as ybsu;
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -91,16 +93,23 @@ async fn main(spawner: Spawner) {
                                                   Level::Low);
     // I2C
     let i2c_contr = p.I2C1;
-    let sda: PIN_14 = p.PIN_14;
-    let scl: PIN_15 = p.PIN_15;
+    let scl = p.PIN_3;
+    let sda = p.PIN_2;
     let hz = 2;
     
-    spawner.spawn(ads1115::task(i2c_contr, sda, scl, hz)).unwrap();
+    spawner.spawn(ads1115::task(i2c_contr, scl, sda, hz)).unwrap();
+    spawner.spawn(ybsu::task(p.USB)).unwrap();
         
     loop {
         let result = ads1115::RESULT.wait().await;
-        let _when = result.time;
-        let _what = result.reading;
+        //let _when = result.time;
+        //let _what = result.reading;
+        log::info!("{},{},{},{},{}", 
+                result.time.as_millis(), 
+                result.reading[0],
+                result.reading[1],
+                result.reading[2],
+                result.reading[3]);
         led.toggle();
     }
 
