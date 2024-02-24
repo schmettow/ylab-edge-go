d100bc49a8b17aabc662fa5318deda29d098f967
schmettow <schmettow@web.de>
2024-02-24 19:26:34 +0100

    + extended configuration to five devices + core 0 takes on I2C1 + sensors are on I2C0 + prepared for shared bus + LSM6 and CO2 sensors added and reading + improved debugging messages (display)

--- src/bin/ylab_dg.rs
+++ src/bin/ylab_dg.rs
@@ -3,10 +3,9 @@
 
 /// CONFIGURATION
 /// 
-/// Adc Ads0 Ads1 Lsm6_1
-static DEV: [bool; 3] = [true, true, false];
-static HZ: [u64; 3] = [5, 10, 30];
-//static HZ: [u64; 3] = [1, 3, 5];
+/// Adc Lsm6 Lsm6 Bmi 
+static DEV: (bool, bool, bool, bool, bool) = (false, false, false, false, true);
+static HZ: (u64, u64, u64, u64, u64) = (1, 97, 97, 5, 1);
 
 use ylab::{ysns::{yco2, yirt_max}, yuio::disp::TEXT as DISP};
 use {defmt_rtt as _, panic_probe as _};
@@ -136,6 +135,7 @@ bind_interrupts!(struct Irqs {
 });
 
 
+
 /// Because the program runs on two cores,
 /// the `init` function is the entry point, not `main`.
 
@@ -144,11 +144,6 @@ fn init() -> ! {
     // Getting hold of the peripherals, 
     // like pins, ADC, and I2C controllers.
     let p = hal::init(Default::default());
-    
-    // Display will use this
-    let i2c0 
-        = i2c::I2c::new_async(p.I2C0, p.PIN_9, p.PIN_8, Irqs, Config::default());
-
     // Spawning a process on the second core
     spawn_core1(p.CORE1, unsafe { &mut CORE1_STACK }, move || {
         // The second core has its own executor, which is 
@@ -160,20 +155,49 @@ fn init() -> ! {
         // to easily switch on and off components at dev time.
 
         executor1.run(|spawner|{
-
-            //#[cfg(feature = "lsm6-grove4")]
-            // Activating the second I2C controller on Grove 4
-            // and spawning a task for the Yxz acceleration sensor
-            if DEV[1]{
-                use ylab::ysns::yirt_max::task as task;
-                let i2c1 
-                    = i2c::I2c::new_async(p.I2C1, p.PIN_7, p.PIN_6,
-                                    Irqs,
-                                    i2c::Config::default());
-                DISP.signal([None, Some("I2C1@Grove".try_into().unwrap()), None,None]);
-                unwrap!(spawner.spawn(task(i2c1, HZ[1])));
+          
+            let i2c_contr  = p.I2C0;
+            match DEV {
+                (_,true, false, false, false) 
+                =>  {   // LSM on Grove 1
+                        let i2c 
+                        = i2c::I2c::new_async(i2c_contr, p.PIN_1, p.PIN_0,
+                                        Irqs,
+                                        i2c::Config::default());
+                        DISP.signal([None, Some("0-1-LSM".try_into().unwrap()), None,None]);
+                        unwrap!(spawner.spawn(ylab::ysns::yxz_lsm6::task(i2c, HZ.1)));
+                    },
+                (_,false, true, false, false) 
+                =>  {   // LSM on Grove 3
+                        let i2c 
+                            = i2c::I2c::new_async(i2c_contr, p.PIN_5, p.PIN_4,
+                                            Irqs,
+                                            i2c::Config::default());
+                        DISP.signal([None, Some("0-3-LSM".try_into().unwrap()), None,None]);
+                        unwrap!(spawner.spawn(ylab::ysns::yxz_lsm6::task(i2c, HZ.2)));
+                        }
+                (_,false, false, true, false) 
+                =>  {   // BMI on Grove 5
+                        let i2c 
+                            = i2c::I2c::new_async(i2c_contr, p.PIN_9, p.PIN_8,
+                                        Irqs,
+                                        i2c::Config::default());
+                        DISP.signal([None, Some("0-3-BMI".try_into().unwrap()), None,None]);
+                        unwrap!(spawner.spawn(ylab::ysns::yxz_bmi160::task(i2c, HZ.3)));  
+                        }
+                (_,false, false, false, true) 
+                =>  {   // CO2 on Grove 5
+                        let i2c 
+                            = i2c::I2c::new_async(i2c_contr, p.PIN_9, p.PIN_8,
+                                        Irqs,
+                                        i2c::Config::default());
+                        DISP.signal([None, Some("0-5-CO2".try_into().unwrap()), None,None]);
+                        unwrap!(spawner.spawn(ylab::ysns::yco2::task(i2c)));  
+                    }
+                _ => {crate::todo!("shared I2C not yet implemented")}
             }
 
+            
         })
     });
 
@@ -183,25 +207,31 @@ fn init() -> ! {
     // + ui events
     // + control flow
     // + sending data
+    
+
     let executor0 = EXECUTOR0.init(Executor::new());
     executor0.run(|spawner| {   
         // task for controlling the led
         unwrap!(spawner.spawn(yled::task(p.PIN_25.degrade())));
         // task for receiving text and put it on an OLED 1306
-        unwrap!(spawner.spawn(ydsp::task(i2c0)));
+        // Display will use I2C1 on 
+        let i2c_contr = p.I2C1;
+        let i2c 
+            = i2c::I2c::new_async(i2c_contr, p.PIN_3, p.PIN_2, Irqs, Config::default());
+        unwrap!(spawner.spawn(ydsp::task(i2c)));
         // task for listening to button presses.
         unwrap!(spawner.spawn(ybtn::task(p.PIN_20.degrade())));
         // task listening for data packeges to send up the line (reverse USB ;)
         unwrap!(spawner.spawn(ybsu::task(p.USB)));
         // task to control sensors, storage and ui
         unwrap!(spawner.spawn(control_task()));
-        if DEV[0]{
+        if DEV.0{
             let adc0: adc::Adc<'_, Async> 
                 = adc::Adc::new( p.ADC, Irqs, adc::Config::default());
             unwrap!(spawner.spawn(
                 yadc::task( adc0, 
                             p.PIN_26, p.PIN_27, p.PIN_28, p.PIN_29, 
-                            HZ[0])));
+                            HZ.0)));
         };
     });
 }
@@ -216,88 +246,77 @@ fn init() -> ! {
 /// 
 /// 
 
-/// To be concurrency-safe, we are using boolean atomics.
+/// To be parallel-safe, we are using boolean atomics.
 /// Atomics are capsules around basic data types that 
 /// efficiently handle concurrent or parallel access.
 pub use core::sync::atomic::Ordering;
+static RLX: Ordering = Ordering::Relaxed;
 
 #[embassy_executor::task]
 async fn control_task() { 
     let mut state = AppState::Record;
-    yadc::RECORD.store(true, Ordering::Relaxed);
-    yxz_lsm6::RECORD.store(true, Ordering::Relaxed);
-    yxz_bmi160::RECORD.store(true, Ordering::Relaxed);
-    yco2::RECORD.store(true, Ordering::Relaxed);
-    yirt_max::RECORD.store(true, Ordering::Relaxed);
+    yadc::RECORD.store(true, RLX);
+    yxz_lsm6::RECORD.store(true, RLX);
+    yxz_bmi160::RECORD.store(true, RLX);
+    yco2::RECORD.store(true, RLX);
+    yirt_max::RECORD.store(true, RLX);
+    //yads0::RECORD.store(true, RLX);
+    //yads1::RECORD.store(true, RLX);
+    
     yled::LED.signal(yled::State::Steady);
-    //DISP.signal([ Some("YLab CTRL... OK".try_into().unwrap()) ,None, None, None]);
-
-    // The main loop of the control task. 
-    // Sometimes I miss the poetry of `while true`
     loop {
-        yadc::RECORD.store(true, Ordering::Relaxed);
-                    //yads0::RECORD.store(true, Ordering::Relaxed);
-                    //yads1::RECORD.store(true, Ordering::Relaxed);
-                    yxz_lsm6::RECORD.store(true, Ordering::Relaxed);
-                    yxz_bmi160::RECORD.store(true, Ordering::Relaxed);
-                    yco2::RECORD.store(true, Ordering::Relaxed);
-        // Listening to the button channel.
-        let btn_1 = ybtn::BTN.wait().await;
-        // When a new event appears in the channel,
-        // a state transition occurs. This is initiated by 
-        // announcing a new state.
-        let next_state = 
-            match (state, btn_1) {
-                (AppState::New,     ybtn::Event::Short) => AppState::Ready,
-                (AppState::Ready,   ybtn::Event::Short) => AppState::Record, 
-                (AppState::Record,  ybtn::Event::Short) => AppState::Ready,
-                (_,                 ybtn::Event::Long)  => AppState::New,
-                (_, _) => state,};
-            
-        // When a new event has been announced we do the transition.
-        // This happens by sending the right messages to all our tasks.
-        if next_state != state {
-            match next_state {
-                AppState::New => {
-                    // LED knows several states, therefore a signal is used
-                    yled::LED.signal(yled::State::Vibrate);
-                    // From here on we are just pulling On/Off switches.
-                    // Again, not all tasks are active. We can still send 
-                    // messages to dangling modules.
-                    yadc::RECORD.store(false, Ordering::Relaxed);
-                    //yads0::RECORD.store(false, Ordering::Relaxed);
-                    //yads1::RECORD.store(false, Ordering::Relaxed);
-                    yxz_lsm6::RECORD.store(false, Ordering::Relaxed);
-                    yxz_bmi160::RECORD.store(false, Ordering::Relaxed);
-                    yirt_max::RECORD.store(false, Ordering::Relaxed);
-                    DISP.signal([ Some("New".try_into().unwrap()), None, None, None]);
-                    },
-                AppState::Ready     => {
-                    yled::LED.signal(yled::State::Blink);
-                    yadc::RECORD.store(false, Ordering::Relaxed);
-                    //yads0::RECORD.store(false, Ordering::Relaxed);
-                    //yads1::RECORD.store(false, Ordering::Relaxed);
-                    yxz_lsm6::RECORD.store(false, Ordering::Relaxed);
-                    yxz_bmi160::RECORD.store(false, Ordering::Relaxed);
-                    yirt_max::RECORD.store(false, Ordering::Relaxed);
-                    DISP.signal([ Some("Ready".try_into().unwrap()),None, None,None]);
-                    
-                    },
-                AppState::Record    => {
-                    yled::LED.signal(yled::State::Steady);
-                    yadc::RECORD.store(true, Ordering::Relaxed);
-                    //yads0::RECORD.store(true, Ordering::Relaxed);
-                    //yads1::RECORD.store(true, Ordering::Relaxed);
-                    yxz_lsm6::RECORD.store(true, Ordering::Relaxed);
-                    yxz_bmi160::RECORD.store(true, Ordering::Relaxed);
-                    yco2::RECORD.store(true, Ordering::Relaxed);
-                    yirt_max::RECORD.store(true, Ordering::Relaxed);
-                    DISP.signal([ Some("Record".try_into().unwrap()),None, None,None]);
+        let event = ybtn::BTN.wait().await;
+        // Only when a new user event appears,
+        // a state transition may occur.
+        if let Some(next_state) = 
+            match (state, event) {
+                (AppState::New,     ybtn::Event::Short) => Some(AppState::Ready),
+                (AppState::Ready,   ybtn::Event::Short) => Some(AppState::Record), 
+                (AppState::Record,  ybtn::Event::Short) => Some(AppState::Ready),
+                (_,                 ybtn::Event::Long)  => Some(AppState::New),
+                (_, _) => None,}
+                {
+                    // When a new event has been announced we do the transition.
+                    // This happens by sending the right messages to all our tasks.
+                    match next_state {
+                        AppState::New => {
+                            // Reset all sensors and vibrate
+                            yled::LED.signal(yled::State::Vibrate);
+                            yadc::RECORD.store(false, RLX);
+                            //yads0::RECORD.store(false, RLX);
+                            //yads1::RECORD.store(false, RLX);
+                            yxz_lsm6::RECORD.store(false, RLX);
+                            yxz_bmi160::RECORD.store(false, RLX);
+                            yirt_max::RECORD.store(false, RLX);
+                            DISP.signal([ Some("New".try_into().unwrap()), None, None, None]);
+                            },
+                        AppState::Ready     => {
+                            // Pause all sensors and blink
+                            yled::LED.signal(yled::State::Blink);
+                            yadc::RECORD.store(false, RLX);
+                            //yads0::RECORD.store(false, RLX);
+                            //yads1::RECORD.store(false, RLX);
+                            yxz_lsm6::RECORD.store(false, RLX);
+                            yxz_bmi160::RECORD.store(false, RLX);
+                            yirt_max::RECORD.store(false, RLX);
+                            DISP.signal([ Some("Ready".try_into().unwrap()),None, None,None]);
+                            },
+                        AppState::Record    => {
+                            // Transmit sensor data and light up
+                            yled::LED.signal(yled::State::Steady);
+                            yadc::RECORD.store(true, RLX);
+                            //yads0::RECORD.store(true, RLX);
+                            //yads1::RECORD.store(true, RLX);
+                            yxz_lsm6::RECORD.store(true, RLX);
+                            yxz_bmi160::RECORD.store(true, RLX);
+                            yco2::RECORD.store(true, RLX);
+                            yirt_max::RECORD.store(true, RLX);
+                            DISP.signal([ Some("Record".try_into().unwrap()),None, None,None]);
+                            }
                     }
+                    state = next_state;
             }
-        state = next_state;
         }
-    }
 
 
 }
