dad8834c358980b21ab218d2b753dfc0543e3f57
schmettow <schmettow@web.de>
2024-04-07 23:09:27 +0200

    + implemented Sample and Ytf

--- src/ylab/mod.rs
+++ src/ylab/mod.rs
@@ -11,10 +11,10 @@ pub use embassy_sync::channel::Channel;
 
 pub use core::sync::atomic::Ordering;
 pub use core::sync::atomic::AtomicBool;
-pub static RLX: Ordering = Ordering::Relaxed;
+//pub static RLX: Ordering = Ordering::Relaxed;
+pub static ORD: Ordering = Ordering::SeqCst;
 
 /*use core::fmt;
-use fmt::Display;
 use fmt::Write;*/
 
 
@@ -23,10 +23,51 @@ pub mod yuio; // YLab UI Output
 pub mod yuii; // YLab UI Input
 pub mod ytfk; // YLab transfer formats & kodices
 
-#[derive(Debug,Eq, PartialEq)]
-pub struct Sample<T> {
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Sample<M, const N: usize>
+    where M: Into<YtfType>
+    {
     pub sensory: u8,
     pub time: Instant,
-    pub read: T,
+    pub read: [M; N],
 }
+
+pub const YTF_LEN: usize = 8;
+pub type YtfType = f64;
+pub type YtfRead = [Option<YtfType>; YTF_LEN];
+
+pub struct Ytf {
+    pub sensory: u8,
+    pub time: Instant,
+    pub read: YtfRead,
+}
+
+impl<M: Into<YtfType>, const N: usize> Into<Ytf> for Sample<M, N> {
+    fn into(self) -> Ytf {
+        let mut out: YtfRead = [None; YTF_LEN];
+        for (i, r) in self.read.into_iter().enumerate() {
+            out[i] = Some(r.into());
+        }
+        Ytf {
+            sensory: self.sensory,
+            time: self.time,
+            read: out
+        }
+    }
+}
+
+impl core::fmt::Display for Ytf {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{}, {}", self.time.as_micros(), self.sensory).unwrap();
+        for r in self.read {
+            match r {
+                Some(v) => {
+                    write!(f, ",{:.3}", v).unwrap();},
+                None => {write!(f, ",").unwrap();}
+            }
+        }
+        write!(f, "")
+    }
+}
+
 
