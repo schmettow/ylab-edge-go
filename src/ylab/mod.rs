58a21a8ebaf9e2f9c9f112c44d309509483a8208
schmettow <schmettow@web.de>
2024-04-25 20:40:33 +0200

    + added trait Sensor + added generic type Reading

--- src/ylab/mod.rs
+++ src/ylab/mod.rs
@@ -2,6 +2,7 @@
 
 pub use embassy_rp as hal;
 pub use embassy_time as time;
+use embedded_hal::i2c::Error;
 pub use time::{Duration, Ticker, Instant, Delay};
 pub use heapless::{Vec, String};
 pub use embassy_sync::mutex::Mutex as Mutex;
@@ -23,13 +24,15 @@ pub mod yuio; // YLab UI Output
 pub mod yuii; // YLab UI Input
 pub mod ytfk; // YLab transfer formats & kodices
 
+pub type Reading<M: Into<YtfType>, const N: usize> = [M; N];
+
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct Sample<M, const N: usize>
     where M: Into<YtfType>
     {
     pub sensory: u8,
     pub time: Instant,
-    pub read: [M; N],
+    pub read: Reading<M, N>,
 }
 
 pub const YTF_LEN: usize = 8;
@@ -42,7 +45,6 @@ pub struct Ytf {
     pub time: Instant,
     pub read: YtfRead,
 }
-
 
 impl<M: Into<YtfType>, const N: usize> Into<Ytf> for Sample<M, N> {
     fn into(self) -> Ytf {
@@ -58,6 +60,7 @@ impl<M: Into<YtfType>, const N: usize> Into<Ytf> for Sample<M, N> {
     }
 }
 
+
 impl core::fmt::Display for Ytf {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "{}, {}", self.time.as_micros(), self.sensory).unwrap();
@@ -69,6 +72,36 @@ impl core::fmt::Display for Ytf {
             }
         }
         write!(f, "")
+    }
+}
+
+#[derive(Debug)]
+enum SensoryError{Init, Read}
+
+trait Sensor<M, const N: usize> {
+    fn prepare(&mut self) -> Result<(), SensoryError>;
+    
+    async fn read(&mut self) -> Result<Reading<M, N>, SensoryError>;
+    
+    async fn sample(&mut self, sensory: u8) -> Result<Sample<M, N>, SensoryError>
+        where M: Into<YtfType> {
+        let reading = self.read().await;
+        match reading {
+            Ok(reading) => Ok(Sample{sensory: sensory, time: Instant::now(), read: reading}),
+            Err(E) => Err(SensoryError::Read),
+        } 
+    }
+    
+    async fn run(&mut self, sensory: u8, interval: Duration) -> ! 
+        where M: Into<YtfType> {
+        let mut ticker = Ticker::every(interval);
+        loop{
+            match self.sample(sensory).await {
+                Ok(sample) => ytfk::bsu::SINK.send(sample.into()).await,
+                Err(_) => {},
+            }
+            ticker.next().await;
+        }
     }
 }
 
